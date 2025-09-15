9fc310a1843fdec13874f57ea983c50ec3dc8729
Ente1408 <entendedor1408@gmail.com>
2025-09-15 17:55:55 -0500

    agregamos el comando al kernel de comandos , y en el controlador main , configuramos el index, para retornar la vista con la informacion inicial
    En el config service, configuramos la key para cuando se consulte la informacion al servicio , sea desde la apikey de mi env

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,62 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\CoinMarketCapService;
+
 
 class MainController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(CoinMarketCapService $cmc)
     {
-        //
+        $data = $cmc->getQuotes("BTC,ETH,BNB");
+
+        return view('dashboard', [
+            'cryptos' => $data['data'] ?? [],
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
